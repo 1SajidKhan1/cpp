7fc8327038a7d9405ea84371d7a58d47af3bc0be
1SajidKhan1 <sajidkhan032004@gmail.com>
2026-02-06 16:16:20 +0530

    different types of input and output

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 
 int main() {
-  std::cout << "Hello, there" << std::endl;
+  int age;
+  std::string name;
+
+  std::cout << "Please type in your Last Name: " << std::endl;
+  std::cin >> name;
+
+  std::cout << "Please type in your age: " << std::endl;
+  std::cin >> age;
+
+  std::cout << "Hello " << name << "! You are " << age << " years old" << std::endl;
 
   return 0;
 }
